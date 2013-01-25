a7fbfa97fda5a44275192d9c118d59af798a59db
mrtgold <timothy.golding@gmail.com>
2013-01-25 05:56:19 -0600

    copy from prev project

---
+++ src/com/oilyliving/tips/MainActivity.java
@@ -0,0 +1,15 @@
+package com.oilyliving.tips;
+
+import android.app.*;
+import android.os.*;
+
+public class MainActivity extends Activity
+{
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState)
+	{
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
