1cf161357767a26f1c87480ac8857287d5faac6d
mrtgold <timothy.golding@gmail.com>
2013-02-25 05:35:39 -0600

    add tips to updater service, update tip ids

--- src/com/oilyliving/tips/data/TipsBootstrap.java
+++ src/com/oilyliving/tips/data/TipsBootstrap.java
@@ -11,21 +11,22 @@ public final class TipsBootstrap
 	{
 		db.beginTransaction();
 		tips.deleteAll();
-		Tip tip1001 = new Tip(1001, context.getString(R.string.rcMrsa), "rc", context.getString(R.string.rcMrsaRef), new Date(0));
+		Tip tip1001 = new Tip(1001, context.getString(R.string.tip1001), "rc", context.getString(R.string.ref1001), new Date(0));
 		tips.insertTip(tip1001);
 
-		Tip tip2001 = new Tip(2001, context.getString(R.string.tip1), "purification");
+		Tip tip2001 = new Tip(2001, context.getString(R.string.tip2001), "purification");
 		tip2001.setEoprPage(149);
 		tip2001.setRgeoPage(187);
 		tips.insertTip(tip2001);
 
-		tips.insertTip(new Tip(3, context.getString(R.string.rcResp), "rc"));
-		tips.insertTip(new Tip(5, context.getString(R.string.tip5), "ylIcon"));
-		tips.insertTip(new Tip(6, context.getString(R.string.tip6), "thievesIcon"));
-		tips.insertTip(new Tip(7, context.getString(R.string.tip7), "frankincense"));
-		tips.insertTip(new Tip(8, context.getString(R.string.tip8), "lavenderIcon"));
-		tips.insertTip(new Tip(9, context.getString(R.string.tip9), "kidScentsIcon"));
-		tips.insertTip(new Tip(10, context.getString(R.string.tip10), "thievesSpray"));
+		tips.insertTip(new Tip(10001, context.getString(R.string.tip10001), "rc"));
+		tips.insertTip(new Tip(4001, context.getString(R.string.tip4001), "ylIcon"));
+		tips.insertTip(new Tip(5001, context.getString(R.string.tip5001), "thievesIcon"));
+		tips.insertTip(new Tip(6001, context.getString(R.string.tip6001), "frankincense"));
+		tips.insertTip(new Tip(7001, context.getString(R.string.tip7001), "lavenderIcon"));
+		tips.insertTip(new Tip(8001, context.getString(R.string.tip8001), "kidScentsIcon"));
+		tips.insertTip(new Tip(9001, context.getString(R.string.tip9001), "thievesSpray"));
+        tips.insertTip(new Tip(10001, context.getString(R.string.tip10001), "rc"));
 		db.setTransactionSuccessful();
 		db.endTransaction();
 	}
